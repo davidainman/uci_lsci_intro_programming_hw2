c3ef14afa461fd583e98eeb19d8473a041636fc6
davidainman <havennah@gmail.com>
2025-11-17 11:12:42 -0800

    Update duplicate_remover.py
    Made the duplicates_list an argument so it can be tested more fully.

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -5,8 +5,12 @@
 # For the example below, the output should be
 # "[1, 4, 3, 2, 5, 7, 9]"
 # 
-def duplicate_remover():
-    duplicates_list = [1, 4, 3, 4, 2, 5, 1, 2, 7, 9, 4]
+# Note that I have put the duplicates_list key into the function
+# as an argument. We will discuss this syntax later. For now,
+# it is a way for me to test your code using different lists.
+# If you want to test your code with different list inputs,
+# simply change the default duplicates_list variable below
+def duplicate_remover(duplicates_list = [1, 4, 3, 4, 2, 5, 1, 2, 7, 9, 4]):
     
     # YOUR CODE GOES HERE
 
