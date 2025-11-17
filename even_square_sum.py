3ab7f669b2a5863da30c8191da58bfc0f991512b
davidainman <havennah@gmail.com>
2025-11-17 11:17:32 -0800

    Update even_square_sum.py
    Making it more testable

--- even_square_sum.py
+++ even_square_sum.py
@@ -8,9 +8,12 @@
 # 
 # For the list below, your code should print 44984
 #
-def even_square_sum():
-    numbers = [1, 62, 3, 57, 26, 8, 101, 200, 43, 20, 11]
-
+# Note that I have put the numbers list into the function
+# as an argument. We will discuss this syntax later. For now,
+# it is a way for me to test your code using different lists.
+# If you want to test your code with different list inputs,
+# simply change the default numbers variable below
+def even_square_sum(numbers = [1, 62, 3, 57, 26, 8, 101, 200, 43, 20, 11]):
     # YOUR CODE GOES HERE
 
 if __name__ == "__main__":
